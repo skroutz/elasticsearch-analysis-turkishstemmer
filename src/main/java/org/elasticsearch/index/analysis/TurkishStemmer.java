35a27378a2cbdd1b097840f2eb254adade7d972b
Tasos Stathopoulos <stathopa@skroutz.gr>
2014-02-13 16:36:08 +0200

    Add the main class of the turkish stemmer

---
+++ src/main/java/org/elasticsearch/index/analysis/TurkishStemmer.java
@@ -0,0 +1,11 @@
+package org.elasticsearch.index.analysis;
+
+public class TurkishStemmer {
+
+  public TurkishStemmer() { }
+
+  public int stem(char s[], int len) {
+    return len;
+  }
+
+}
--- src/main/java/org/elasticsearch/index/analysis/TurkishStemmerTokenFilter.java
+++ src/main/java/org/elasticsearch/index/analysis/TurkishStemmerTokenFilter.java
@@ -2,17 +2,31 @@ package org.elasticsearch.index.analysis;
 
 import java.io.IOException;
 import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.TokenFilter;
+import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
+import org.apache.lucene.analysis.tokenattributes.KeywordAttribute;
 
-public class TurkishStemmerTokenFilter extends TokenStream {
-  
+public class TurkishStemmerTokenFilter extends TokenFilter {
+  private final TurkishStemmer stemmer = new TurkishStemmer();
+  private final CharTermAttribute termAtt = addAttribute(CharTermAttribute.class);
+  private final KeywordAttribute keywordAttr = addAttribute(KeywordAttribute.class);
+
+
   public TurkishStemmerTokenFilter(TokenStream input) {
     super(input);
   }
 
   @Override
   public boolean incrementToken() throws IOException {
-    // TODO Auto-generated method stub
-    return false;
+    if (input.incrementToken()) {
+      if (!keywordAttr.isKeyword()) {
+        final int newlen = stemmer.stem(termAtt.buffer(), termAtt.length());
+        termAtt.setLength(newlen);
+      }
+      return true;
+    } else {
+      return false;
+    }
   }
 
 }
