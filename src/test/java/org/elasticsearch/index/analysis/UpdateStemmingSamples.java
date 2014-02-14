cae195748910ab8c902e68aff120674eaf8db860
Tasos Stathopoulos <stathopa@skroutz.gr>
2014-02-14 12:09:55 +0200

    Add a task that updates the stemming samples after running tests

---
+++ src/test/java/org/elasticsearch/index/analysis/UpdateStemmingSamples.java
@@ -0,0 +1,42 @@
+package org.elasticsearch.index.analysis;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.util.List;
+import org.apache.lucene.analysis.util.WordlistLoader;
+import org.apache.lucene.util.IOUtils;
+
+public class UpdateStemmingSamples {
+  private final static TurkishStemmer stemmer = new TurkishStemmer();
+
+  public static void main(String args[])
+      throws java.io.IOException, java.io.FileNotFoundException
+  {
+    List<String> lines = WordlistLoader.getLines(
+        new FileInputStream("src/test/resources/stemming_samples.txt"),
+        IOUtils.CHARSET_UTF_8);
+
+    char[] token;
+    int tokenLength, stemLength;
+    String stem;
+    File file = new File("src/test/resources/stemming_samples.txt");
+    FileWriter fileWriter = new FileWriter(file.getAbsoluteFile());
+    BufferedWriter writer = new BufferedWriter(fileWriter);
+
+    try {
+      for(String line : lines) {
+        String[] sample =  line.split(",");
+        token = sample[0].toCharArray();
+        tokenLength = sample[0].length();
+        stemLength = stemmer.stem(token, tokenLength);
+        stem = new String(token, 0, stemLength);
+        writer.write(sample[0] + "," + stem);
+        writer.newLine();
+      }
+    } finally {
+      writer.close();
+    }
+  }
+}
