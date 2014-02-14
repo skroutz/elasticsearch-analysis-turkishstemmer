edcfb1891b4e8d70b7f377bf4b3f1d5b5cea6b8b
Tasos Stathopoulos <stathopa@skroutz.gr>
2014-02-14 12:13:45 +0200

    Add test for TurkishStemmer class

---
+++ src/test/java/org/elasticsearch/index/analysis/TurkishStemmerTest.java
@@ -0,0 +1,35 @@
+package org.elasticsearch.index.analysis;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TurkishStemmerTest {
+  private final TurkishStemmer stemmer = new TurkishStemmer();
+
+  /**
+   * a sample of greek words that should be stemmed by the Skroutz Greek stemmer.
+   */
+  private static final String[] words = { };
+
+  /**
+   * the stems that should be returned by the stemmer for the above words.
+   */
+  private static final String[] stems = { };
+
+  private char[] token;
+  private String stem;
+  private int tokenLength, stemLength;
+
+  @Test
+  public void testTurkishStemmer() {
+    for (int i = 0; i < words.length; i++) {
+      token = words[i].toCharArray();
+      tokenLength = words[i].length();
+      stemLength = stemmer.stem(token, tokenLength);
+      stem = new String(token, 0, stemLength);
+
+      Assert.assertEquals(stem, stems[i]);
+    }
+  }
+
+}
