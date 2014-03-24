76b69078861011dfeb0380cfae3c9ad3c8934d8b
Tasos Stathopoulos <stathopa@skroutz.gr>
2014-03-24 16:32:50 +0200

    Add state interface to DRY source as much as possible

---
+++ src/main/java/org/elasticsearch/index/analysis/stemmer/turkish/states/State.java
@@ -0,0 +1,33 @@
+package org.elasticsearch.index.analysis.stemmer.turkish.states;
+
+import java.util.List;
+
+import org.elasticsearch.index.analysis.stemmer.turkish.transitions.Transition;
+
+public interface State {
+
+  /**
+   * Checks if the state is an initial state.
+   *
+   * @return whether or not this state is initial
+   */
+  boolean initialState();
+  /**
+   * Checks if the state is final.
+   *
+   * @return whether or not this state is final
+   */
+  boolean finalState();
+  /**
+   * Adds possible transitions from the current state to other states
+   * about a word to a given list.
+   *
+   * @param word a word to search transitions for
+   * @param transitions the initial list to add transitions
+   * @param rollbackWord a given rollback word
+   * @param marked whether to mark the transitions as marked
+   */
+  void addTransitions(String word, List<Transition> transitions,
+      String rollbackWord, boolean marked);
+
+}
