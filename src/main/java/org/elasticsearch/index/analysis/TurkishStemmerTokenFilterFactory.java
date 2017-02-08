71c84fed90c53881131458d6d253117793dc88a1
Bill Kolokithas <kolokithas.b@gmail.com>
2017-02-08 18:40:41 +0200

    Move from dynamic settings to full-fledged class
    As a result, the 'index' argument from the constructor is dropped (and
    so is the import).
    
    You can find the reasoning behind this change in the description of the
    following commit:
    https://github.com/elastic/elasticsearch/commit/66d5d0c4f2de78371ccb5447e503d9d30b6b51c4

--- src/main/java/org/elasticsearch/index/analysis/TurkishStemmerTokenFilterFactory.java
+++ src/main/java/org/elasticsearch/index/analysis/TurkishStemmerTokenFilterFactory.java
@@ -12,8 +12,7 @@ import org.elasticsearch.common.inject.Inject;
 import org.elasticsearch.common.inject.assistedinject.Assisted;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.env.Environment;
-import org.elasticsearch.index.Index;
-import org.elasticsearch.index.settings.IndexSettingsService;
+import org.elasticsearch.index.IndexSettings;
 
 public class TurkishStemmerTokenFilterFactory extends AbstractTokenFilterFactory {
 
@@ -22,14 +21,12 @@ public class TurkishStemmerTokenFilterFactory extends AbstractTokenFilterFactory
   private final CharArraySet vowelHarmonyExceptions;
   private final CharArraySet averageStemSizeExceptions;
 
-
   @Inject
-  public TurkishStemmerTokenFilterFactory(Index index,
-      IndexSettingsService indexSettings,
+  public TurkishStemmerTokenFilterFactory(IndexSettings indexSettings,
       Environment env, @Assisted String name,
       @Assisted Settings settings) {
 
-    super(index, indexSettings.getSettings(), name, settings);
+    super(indexSettings, name, settings);
     this.protectedWords = parseProtectedWords(env, settings,
         "protected_words_path");
     this.vowelHarmonyExceptions = parseVowelHarmonyExceptions(env, settings,
