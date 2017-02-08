e5772596691728a6f245940ff7e65aecb85d5b2c
Bill Kolokithas <kolokithas.b@gmail.com>
2017-02-08 19:05:09 +0200

    Bump dependencies to newer versions
    The log4j is needed to fix the following logger error:
    "class file for org.apache.logging.log4j.Logger not found"

--- src/main/java/org/elasticsearch/index/analysis/TurkishStemmer.java
+++ src/main/java/org/elasticsearch/index/analysis/TurkishStemmer.java
@@ -16,7 +16,7 @@ import org.apache.commons.lang3.StringUtils; // Apache StringUtils
 import org.apache.lucene.analysis.CharArraySet;
 import org.apache.lucene.analysis.util.WordlistLoader;
 import org.apache.lucene.util.IOUtils;
-import org.elasticsearch.common.logging.ESLogger;
+import org.apache.logging.log4j.Logger;
 import org.elasticsearch.common.logging.Loggers;
 import org.elasticsearch.index.analysis.stemmer.turkish.states.DerivationalState;
 import org.elasticsearch.index.analysis.stemmer.turkish.states.NominalVerbState;
@@ -33,9 +33,10 @@ import com.google.common.base.CharMatcher;   // Guava
 public class TurkishStemmer {
 
   /**
-   * Elasticsearch logger.
+   * Logger.
    */
-  protected final ESLogger logger = Loggers.getLogger("turkish-stemmer");
+  protected final Logger logger = Loggers.getLogger("turkish-stemmer");
+
   /**
    * The turkish characters. They are used for skipping not turkish words.
    */
